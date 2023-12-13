501df028ea8b77bb8bd483e32b7066f2330224e2
AngleSad <118389540+AngleSad@users.noreply.github.com>
2023-12-13 14:09:59 +0100

    1. Minigame working progress

---
+++ app/src/main/java/com/azaldev/garden/MiniGame.kt
@@ -0,0 +1,12 @@
+package com.azaldev.garden
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MiniGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mini_game)
+    }
+}
